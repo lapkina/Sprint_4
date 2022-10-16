486622b40d23e54f8df5aa9f90bb6ff4fe392e44
Lapkina <lapshina90@mail.ru>
2022-10-16 21:28:53 +0400

    Second commit

--- src/test/java/ru/yandex/praktikum/Samokat/TestsAccordion.java
+++ src/test/java/ru/yandex/praktikum/Samokat/TestsAccordion.java
@@ -1,13 +1,16 @@
 package ru.yandex.praktikum.Samokat;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.hamcrest.MatcherAssert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.junit.Test;
 import org.junit.After;
 import org.junit.Before;
 import ru.yandex.praktikum.Samokat.PO.MainPage;
+import static org.hamcrest.CoreMatchers.containsString;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 
 
 
@@ -26,26 +29,131 @@ public class TestsAccordion {
 
     public void TestAnswerAboutCost() {
 
-        WebDriver driver = new ChromeDriver();
+        //WebDriver driver = new ChromeDriver();
         // переход на страницу тестового приложения
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        driver = new ChromeDriver( options );
         driver.get("https://qa-scooter.praktikum-services.ru");
 
         MainPage objMainPage = new MainPage(driver);
-        // mainPage.scrollToAccordion();
+        objMainPage.clickForCookies();
         objMainPage.clickQuestionsCost();
         objMainPage.getAnswerAboutCost();
+        MatcherAssert.assertThat(objMainPage.getAnswerAboutCost(), containsString("Сутки — 400 рублей. Оплата курьеру — наличными или картой."));
     }
 
     @Test
     public void TestAnswerAboutMany() {
-        WebDriver driver = new ChromeDriver();
-        // переход на страницу тестового приложения
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        driver = new ChromeDriver( options );
         driver.get("https://qa-scooter.praktikum-services.ru");
 
         MainPage objMainPage = new MainPage(driver);
-        //mainPage.scrollToAccordion();
+        objMainPage.clickForCookies();
         objMainPage.clickQuestionsMany();
         objMainPage.getAnswerAboutMany();
+        MatcherAssert.assertThat(objMainPage.getAnswerAboutMany(), containsString("Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим."));
+    }
+    @Test
+
+    public void TestAnswerAboutTime() {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        driver = new ChromeDriver( options );
+        driver.get("https://qa-scooter.praktikum-services.ru");
+
+        MainPage objMainPage = new MainPage(driver);
+        objMainPage.clickForCookies();
+        objMainPage.clickQuestionsTime();
+        objMainPage.getAnswerAboutTime();
+        MatcherAssert.assertThat(objMainPage.getAnswerAboutTime(), containsString("Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30."));
+    }
+
+
+    @Test
+
+    public void TestAnswerAboutToday() {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        driver = new ChromeDriver( options );
+        driver.get("https://qa-scooter.praktikum-services.ru");
+
+        MainPage objMainPage = new MainPage(driver);
+        objMainPage.clickForCookies();
+        objMainPage.clickQuestionsToday();
+        objMainPage.getAnswerAboutToday();
+        MatcherAssert.assertThat(objMainPage.getAnswerAboutToday(), containsString("Только начиная с завтрашнего дня. Но скоро станем расторопнее."));
+
+    }
+
+    @Test
+
+    public void TestAnswerAboutEarlier() {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        driver = new ChromeDriver( options );
+        driver.get("https://qa-scooter.praktikum-services.ru");
+
+        MainPage objMainPage = new MainPage(driver);
+        objMainPage.clickForCookies();
+        objMainPage.clickQuestionsEarlier();
+        objMainPage.getAnswerAboutEarlier();
+        MatcherAssert.assertThat(objMainPage.getAnswerAboutEarlier(), containsString("Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010."));
+
+    }
+
+    @Test
+
+    public void TestAnswerAboutCharger() {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        driver = new ChromeDriver( options );
+        driver.get("https://qa-scooter.praktikum-services.ru");
+
+        MainPage objMainPage = new MainPage(driver);
+        objMainPage.clickForCookies();
+        objMainPage.clickQuestionsCharger();
+        objMainPage.getAnswerAboutCharger();
+        MatcherAssert.assertThat(objMainPage.getAnswerAboutCharger(), containsString("Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится."));
+    }
+
+    @Test
+
+    public void TestAnswerAboutCancel() {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        driver = new ChromeDriver( options );
+        driver.get("https://qa-scooter.praktikum-services.ru");
+
+        MainPage objMainPage = new MainPage(driver);
+        objMainPage.clickForCookies();
+        objMainPage.clickQuestionsCancel();
+        objMainPage.getAnswerAboutCancel();
+        MatcherAssert.assertThat(objMainPage.getAnswerAboutCancel(), containsString("Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои."));
+    }
+
+    @Test
+
+    public void TestAnswerAboutMKAD() {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        driver = new ChromeDriver( options );
+        driver.get("https://qa-scooter.praktikum-services.ru");
+
+        MainPage objMainPage = new MainPage(driver);
+        objMainPage.clickForCookies();
+        objMainPage.clickQuestionsMKAD();
+        objMainPage.getAnswerAboutMKAD();
+        MatcherAssert.assertThat(objMainPage.getAnswerAboutMKAD(), containsString("Да, обязательно. Всем самокатов! И Москве, и Московской области."));
     }
 
     @After
--- src/test/java/ru/yandex/praktikum/Samokat/TestsOrder.java
+++ src/test/java/ru/yandex/praktikum/Samokat/TestsOrder.java
@@ -1,6 +1,7 @@
 package ru.yandex.praktikum.Samokat;
 
 import static org.hamcrest.CoreMatchers.containsString;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -14,92 +15,92 @@ import org.hamcrest.MatcherAssert;
 import org.junit.Before;
 
 
+public class TestsOrder {
 
-    public class  TestsOrder {
+    private WebDriver driver;
 
-        private WebDriver driver;
+    @Before
 
-        @Before
-
-        public void startUp() {
-            WebDriverManager.chromedriver().setup();
-        }
-
-        @Test
-        //сначала на первую кнопку
-        public void TestOderUpPositive() {
-
-            WebDriver driver = new ChromeDriver();
-            // переход на страницу тестового приложения
-            driver.get("https://qa-scooter.praktikum-services.ru");
-
-            MainPage objMainPage = new MainPage(driver);
-            objMainPage.clickForOrderUp();
-
-            FirstPageOfOrders objFirstPageOfOrders = new FirstPageOfOrders(driver);
-
-            objFirstPageOfOrders.setNameInput("Котик");
-            objFirstPageOfOrders.setLastNameInput("Кокосик");
-            objFirstPageOfOrders.setAdressInput("Домик на опушке");
-            objFirstPageOfOrders.setMetroInput("Лубянка");
-            objFirstPageOfOrders.clickMetroSudgestLubyanka();
-            objFirstPageOfOrders.setPhoneInput("79811111111");
-            objFirstPageOfOrders.clickForContinueButton();
-
-            SecondPageOfOrders objSecondPageOfOrders = new SecondPageOfOrders(driver);
-
-            objSecondPageOfOrders.setDateInput("22.10.2021");
-            objSecondPageOfOrders.clickGreyCheckBox();
-            objSecondPageOfOrders.clickTimeInput();
-            objSecondPageOfOrders.clickDropDown1days();
-            objSecondPageOfOrders.clickOrderButtonByOrder();
-            objSecondPageOfOrders.clickButtonYesByOrder();
-            objSecondPageOfOrders.getTextAboutOrder();
-
-            MatcherAssert.assertThat(objSecondPageOfOrders.getTextAboutOrder(), containsString("Заказ оформлен"));
-
-
-        }
-
-        //теперь на вторую кнопку
-        @Test
-        public void TestOderDownPositive() {
-
-            WebDriver driver = new ChromeDriver();
-            // переход на страницу тестового приложения
-            driver.get("https://qa-scooter.praktikum-services.ru");
-
-            MainPage objMainPage = new MainPage(driver);
-            objMainPage.clickForOrderDown();
-
-            FirstPageOfOrders objFirstPageOfOrders = new FirstPageOfOrders(driver);
-
-            objFirstPageOfOrders.setNameInput("Иван");
-            objFirstPageOfOrders.setLastNameInput("Неиван");
-            objFirstPageOfOrders.setAdressInput("мой адрес не дом и не улица");
-            objFirstPageOfOrders.setMetroInput("Аэропорт");
-            objFirstPageOfOrders.clickMetroSudgestAeroport();
-            objFirstPageOfOrders.setPhoneInput("89818111111");
-            objFirstPageOfOrders.clickForContinueButton();
-
-            SecondPageOfOrders objSecondPageOfOrders = new SecondPageOfOrders(driver);
-
-            objSecondPageOfOrders.setDateInput("15.12.2021");
-            objSecondPageOfOrders.clickBlackCheckBox();
-            objSecondPageOfOrders.clickTimeInput();
-            objSecondPageOfOrders.clickDropDown3days();
-            objSecondPageOfOrders.clickOrderButtonByOrder();
-            objSecondPageOfOrders.clickButtonYesByOrder();
-            objSecondPageOfOrders.getTextAboutOrder();
-
-            MatcherAssert.assertThat(objSecondPageOfOrders.getTextAboutOrder(), containsString("Заказ оформлен"));
-
-            }
-        @After
-        public void teardown () {
-
-            driver.quit();
-        }
+    public void startUp() {
+        WebDriverManager.chromedriver().setup();
     }
 
+    @Test
+    //сначала на первую кнопку
+    public void TestOderUpPositive() {
 
+        WebDriver driver = new ChromeDriver();
+        // переход на страницу тестового приложения
+        driver.get("https://qa-scooter.praktikum-services.ru");
+
+        MainPage objMainPage = new MainPage(driver);
+        objMainPage.clickForOrderUp();
+
+        FirstPageOfOrders objFirstPageOfOrders = new FirstPageOfOrders(driver);
+        objFirstPageOfOrders.clickNameInput();
+        objFirstPageOfOrders.setNameInput("Котик");
+        objFirstPageOfOrders.setLastNameInput("Кокосик");
+        objFirstPageOfOrders.setAdressInput("Домик на опушке");
+        objFirstPageOfOrders.setMetroInput("Лубянка");
+        objFirstPageOfOrders.clickMetroSudgestLubyanka();
+        objFirstPageOfOrders.setPhoneInput("79811111111");
+        objFirstPageOfOrders.clickForContinueButton();
+
+        SecondPageOfOrders objSecondPageOfOrders = new SecondPageOfOrders(driver);
+
+        objSecondPageOfOrders.setDateInput("22.10.2021");
+        objSecondPageOfOrders.clickGreyCheckBox();
+        objSecondPageOfOrders.clickTimeInput();
+        objSecondPageOfOrders.clickDropDown1days();
+        objSecondPageOfOrders.clickOrderButtonByOrder();
+        objSecondPageOfOrders.clickButtonYesByOrder();
+        objSecondPageOfOrders.getTextAboutOrder();
+
+        MatcherAssert.assertThat(objSecondPageOfOrders.getTextAboutOrder(), containsString("Заказ оформлен"));
+
+
+    }
+
+    //теперь на вторую кнопку
+    @Test
+    public void TestOderDownPositive() {
+
+        WebDriver driver = new ChromeDriver();
+        // переход на страницу тестового приложения
+        driver.get("https://qa-scooter.praktikum-services.ru");
+
+        MainPage objMainPage = new MainPage(driver);
+        objMainPage.clickForCookies();
+        objMainPage.clickForOrderDown();
+
+
+        FirstPageOfOrders objFirstPageOfOrders = new FirstPageOfOrders(driver);
+
+        objFirstPageOfOrders.setNameInput("Иван");
+        objFirstPageOfOrders.setLastNameInput("Неиван");
+        objFirstPageOfOrders.setAdressInput("мой адрес не дом и не улица");
+        objFirstPageOfOrders.setMetroInput("Аэропорт");
+        objFirstPageOfOrders.clickMetroSudgestAeroport();
+        objFirstPageOfOrders.setPhoneInput("89818111111");
+        objFirstPageOfOrders.clickForContinueButton();
+
+        SecondPageOfOrders objSecondPageOfOrders = new SecondPageOfOrders(driver);
+
+        objSecondPageOfOrders.setDateInput("15.12.2021");
+        objSecondPageOfOrders.clickBlackCheckBox();
+        objSecondPageOfOrders.clickTimeInput();
+        objSecondPageOfOrders.clickDropDown3days();
+        objSecondPageOfOrders.clickOrderButtonByOrder();
+        objSecondPageOfOrders.clickButtonYesByOrder();
+        objSecondPageOfOrders.getTextAboutOrder();
+
+        MatcherAssert.assertThat(objSecondPageOfOrders.getTextAboutOrder(), containsString("Заказ оформлен"));
+
+    }
+
+    @After
+    public void teardown() {
+
+        driver.quit();
+    }
+}
